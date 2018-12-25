25f3f3e98933f7aa55e96649db868d4f52829766
Matheus Icaro <matheus.martins@hop.digital>
2018-12-25 19:19:48 -0200

    Add router to components in App.

---
+++ src/app/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router'
+
+import { connect } from 'react-redux'
+import { initialData } from '../actions'
+
+import Home from './pages/Home'
+
+class App extends Component {
+
+  componentDidMount() {
+    this.props.dispatch(initialData());
+  }
+
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          
+          {/* 
+          TODO...: Quando não achar nenhuma rota acima *
+            <Route component={NotFoundScreen} /> 
+          */}
+        </Switch>
+      </div>
+    )
+  }
+}
+
+export default connect()(App)
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './app/components/App'
+import App from './app/App'
 import { Provider } from 'react-redux' // << Add
 import { createStore } from 'redux'    // << Add
 import reducer from './reducers'       // << Add
 import middleware from './middleware'
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { BrowserRouter } from 'react-router-dom'
 
 const store = createStore(reducer, composeWithDevTools(middleware))     // << Add
 
 ReactDOM.render(
-  <Provider store={store}>             
-    <App />
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 ) 
