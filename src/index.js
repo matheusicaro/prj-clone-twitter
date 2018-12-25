0bf0cf840901ac183697805b92df270c6decc41d
Matheus Icaro <matheus.martins@hop.digital>
2018-12-25 16:38:10 -0200

    Add plugin debug Redux in Chrome.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './components/App'
+import App from './app/components/App'
 import { Provider } from 'react-redux' // << Add
 import { createStore } from 'redux'    // << Add
 import reducer from './reducers'       // << Add
 import middleware from './middleware'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducer, middleware)     // << Add
+const store = createStore(reducer, composeWithDevTools(middleware))     // << Add
 
 ReactDOM.render(
-  <Provider store={store}>             {/* << Add  */}
+  <Provider store={store}>             
     <App />
   </Provider>,
   document.getElementById('root')
